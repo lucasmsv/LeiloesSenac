064745b149435a0f12108b3c1012a3be36463c4c
Lucas <>
2023-12-27 17:45:32 -0300

    Definindo tela inicial do projeto

---
+++ LeiloesTDSat/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new cadastroVIEW().setVisible(true);
+            }
+        });
+    }
+}
